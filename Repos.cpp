3572b2fa7b5220a9c1395e94d304f19f0f0d5f28
Ana-Gomez03 <78995283+Ana-Gomez03@users.noreply.github.com>
2021-03-23 10:07:27 -0600

    Add files via upload

---
+++ Repos.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+    //Ana Elisa Felipe Gomez 1607921
+    int n1;
+    int res;
+  std::cout << "Ingrese numero de estudiantes:";
+  std::n1;
+  if (n1 = 4 && 4 * n1){
+      cout << "Deve ser virtual:"; 
+  } else {
+      cout << "Deve ser presencial:";
+  }
+
+  return 0;
+}
